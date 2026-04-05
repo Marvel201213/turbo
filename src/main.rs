7f2c61b8c9883b923728629eb373af7ee10cde1d
Akshath Sivachidhambaram <akshath.siva@gmail.com>
2026-04-04 20:49:38 -0400

    improved command line handling, with robust error handling, modularization, and utilization of chess crate for board state handling with hard-coded move output

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use clap::{Parser};
+use chess::{Board, BoardStatus, Square, ChessMove};
+use std::str::FromStr;
+use std::error::Error;
+use std::process;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -7,9 +11,39 @@ struct Args {
     // Defaults to 5 as the standard depth, follow cargo run
     // with -- before using depth flag and inputting number
     depth: i32,
+    // FEN-String encoding
+    #[arg(required = true)]
+    fen: String,
+}
+struct Move {
+    status: BoardStatus,
+    chess_move: Option<ChessMove>,
 }
 
 fn main() {
     let args = Args::parse();
-    println!("{}", args.depth);
+    if let Err(e) = run(args) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+    
+
+}
+
+fn run(config: Args) -> Result<Move, Box<dyn Error>> {
+    println!("We shall analyze the position at depth {}.", config.depth);
+    let board_position = Board::from_str(&config.fen).map_err(|e| format!("FEN is invalid: {e}"))?;
+    let status = board_position.status();
+    if status != BoardStatus::Ongoing {
+        println!("Game has already concluded {:?}", status);
+        return Ok(Move {
+            status, 
+            chess_move: None,
+        });
+    }
+    let best_move = ChessMove::new(Square::E2, Square::E4, None);
+    Ok(Move {
+        status, 
+        chess_move: Some(best_move),
+    })
 }
