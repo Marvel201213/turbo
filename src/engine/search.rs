14e3acc7c121c02a63ed6069283dfb49bc2f9b0a
Akshath Sivachidhambaram <akshath.siva@gmail.com>
2026-04-24 20:23:09 -0400

    find best move logic implementation

--- src/engine/search.rs
+++ src/engine/search.rs
@@ -1,5 +1,6 @@
 use crate::engine::config::{EvalConfig};
 use chess::{ChessMove, Board, MoveGen, Piece};
+use super::evaluation::evaluate_board;
 
 const MATE_VALUE: u32 = 20000;
 pub struct Searcher<'a> {
@@ -13,7 +14,26 @@ impl<'a> Searcher<'a> {
     }
 
     pub fn find_best_move(&mut self, board: &mut Board, depth: usize) -> (Option<ChessMove>, i32) {
-
+        let mut best_move = None;
+        // Alpha serves as the maximizing score found so far for the current player to move, not int min because int overflow when negated
+        let mut alpha = -i32::MAX;
+        // Beta is the minimizing score so far for the opposition (assuming the opposition plays competently,
+        // they will not lead you down a path with a score better for you than beta, allowing pruning)
+        let beta = i32::MAX;
+        if depth == 0 {
+            return (None, evaluate_board(board, self.config));
+        }
+        let movegen = self.order_moves(board);
+        for  m in movegen {
+            let mut next_board = Board::default();
+            board.make_move(m, &mut next_board);
+            let score = -self.negamax(&mut next_board, depth - 1, -beta, -alpha);
+            if score > alpha {
+                alpha = score;
+                best_move = Some(m);
+            }
+        }
+        (best_move, alpha)
     }
 
     fn negamax(&mut self, board: &mut Board, depth: usize, mut alpha: i32, beta: i32) -> i32 {
@@ -43,12 +63,16 @@ impl<'a> Searcher<'a> {
 
     fn order_moves(&mut self, board: &mut Board) -> Vec<ChessMove> {
         let mut movegen: Vec<ChessMove> = MoveGen::new_legal(board).collect();
-        let board_ref: &Board = board;
-        movegen.sort_by_key(|m| -self.score_move(m, board_ref));
-        movegen
+        // Basically stores tuple with evaluation using the move scoring with MVV-LVA
+        let mut score_moves: Vec<(ChessMove, i32)> = movegen.into_iter().map(|m| {
+            let score = self.score_move(&m, board);
+            (m, score)
+        }).collect();
+        score_moves.sort_by_key(|&(_, score)| -score);
+        score_moves.into_iter().map(|(m, _)| m).collect()
     }
 
-    fn score_move(&mut self, mv: &ChessMove, board: &Board) -> i32 {
+    fn score_move(&self, mv: &ChessMove, board: &Board) -> i32 {
         let mut score = 0;
         let attacker = board.piece_on(mv.get_source()).unwrap();
         if let Some(victim) = board.piece_on(mv.get_dest()) {
@@ -62,7 +86,7 @@ impl<'a> Searcher<'a> {
         score
     }
 
-    fn capture_checker(&mut self, board: &mut Board, mut alpha: i32, beta: i32) -> i32 {
+    fn capture_checker(&self, board: &Board, mut alpha: i32, beta: i32) -> i32 {
         1
     }
 }
